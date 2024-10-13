83b39c2d62c013b00d0728f27b138f10b4836a5b
Daniel Lima <dnlima77237@gmail.com>
2024-10-13 10:03:36 -0300

    Updt

--- math_functions/factorial.py
+++ math_functions/factorial.py
@@ -1,5 +1,13 @@
+def factorial_iterative(n: int) -> int:
+    result = 1
+    for i in range(2, n+1):
+        result *= i
+    return result
+
 def factorial(n: int) -> int:
-    if n == 1:
+    # equivalente a
+    # if n == 1 or n == 0
+    if n in [0, 1]:
         return n
     return n * factorial(n - 1)
 
